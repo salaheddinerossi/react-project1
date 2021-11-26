1d9fa7430f729414d9e08ebdc58656211af53b20
Salah <salaheddine.rossi@etu.uae.ac.ma>
2021-11-26 17:33:14 +0100

    seconde commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,25 @@
 import Alink from './Alink';
+import { useLocation } from 'react-router';
 
 
 function Navbar() {
+    var a,b,c;
+    const location = useLocation();
+    if (location.pathname == '/'){
+        a='titlecolor'
+    }
+    if(location.pathname == '/blogs'){
+        b='titlecolor'
+    }
+    if(location.pathname == '/create'){
+        c='titlecolor'
+    }
+
     return (
         <ul className="flex-1 flex justify-center ml-auto  navbarcolor m-1.5 py-1">
-            <Alink route="/" textcolor="titlecolor" name="Home"/>
-            <Alink route="/blogs" name="Navigate"/>
-            <Alink route="/create" name="Create"/>
+            <Alink route="/" textcolor={a} name="Home"/>
+            <Alink route="/blogs" textcolor={b} name="Navigate"/>
+            <Alink route="/create" textcolor={c} name="Create"/>
         </ul>
     );
   }
